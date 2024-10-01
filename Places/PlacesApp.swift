da784a84ce0dd5b010115621384344cac38f4883
Paulo Eduardo Correa <paulo.correa@sytac.io>
2024-10-01 13:06:36 +0200

    Initial Commit

---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Paulo Correa on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
